edfc2a906c985c018b52649f28c7154e62d3a430
Alexey <redspirit@live.ru>
2024-11-13 20:40:30 +0300

    add commands

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-const StaticServer = require('static-server');
-const build = require('./lib/build');
-const { serverEnable, serverPort, destinationDir} = require('./lib/config');
+const config = require('./lib/config');
 
-if (serverEnable) {
-    // run http static server
+const cmd = process.argv[2];
+const port = parseInt(process.argv[3] || config.serverPort);
+
+if (cmd === 'build') {
+    // create html files
+    console.log('Build site...');
+    const build = require('./lib/build');
+
+    build.start().then(() => {
+        console.log('Done!');
+    }, (error) => {
+        console.log('Build error', error);
+    });
+
+} else if (cmd === 'serve') {
+    // run static server
+
+    const StaticServer = require('static-server');
     let serv = new StaticServer({
-        rootPath: destinationDir,
-        port: serverPort,
+        rootPath: config.destinationDir,
+        port: port,
     });
     serv.start(() => {
-        console.log('Static server listening on', serverPort);
+        console.log('Static server listening on', port);
     });
-}
 
-build.start().then((result) => {
-    console.log('Build is ok');
-}, (error) => {
-    console.log('Build error', error);
-});
+} else if (cmd === 'deploy') {
+    // build + serve
+
+
+} else {
+    module.exports = {}
+}
